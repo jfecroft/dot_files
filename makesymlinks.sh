1c0e6abec4c880871631b17be2a47a929ef141aa
jfecroft <j.f.e.croft@gmail.com>
2016-01-28 08:59:26 -0800

    fix copying of old dotfiles
    previously would copy the symlink - whihc would point to the new file when
    changed!

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 dir=$PWD                   # dotfiles directory
-olddir=~/dir/dot_files_old             # old dotfiles backup directory
+olddir=~/dot_files_old             # old dotfiles backup directory
 files="bashrc vimrc vim gitconfig git-completion.bash tmux.conf gitignore_global bash_profile config"    # list of files/folders to symlink in homedir
 
 ##########
@@ -25,7 +25,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir
+    cp -r  $file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
